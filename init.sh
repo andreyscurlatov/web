a1deee8dbd099eb3096d94116b8138493eaf3c8e
andreyscurlatov <andreyscurlatov@gmail.com>
2020-06-20 10:11:03 +0300

    Add gunicorn.conf

--- init.sh
+++ init.sh
@@ -2,7 +2,7 @@
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-sudo ln -sf /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
+sudo ln -sf /home/box/web/etc/gunicorn.conf.py   /etc/gunicorn.d/test.py
 sudo /etc/init.d/gunicorn restart
 #﻿sudo /etc/init.d/mysql start﻿
 
