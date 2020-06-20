1bc618afda08f7b7d38162c188a74d1b2b1aa42c
andreyscurlatov <andreyscurlatov@gmail.com>
2020-06-20 18:09:30 +0300

    hello.py

--- init.sh
+++ init.sh
@@ -3,7 +3,7 @@ sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 sudo ln -sf /home/box/web/etc/gunicorn.conf.py   /etc/gunicorn.d/test.py
-sudo /etc/init.d/gunicorn restart
+sudo gunicorn -c /etc/gunicorn.d/test.py hello:app
 #﻿sudo /etc/init.d/mysql start﻿
 
 
