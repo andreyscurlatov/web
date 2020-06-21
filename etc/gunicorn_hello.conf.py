7442666b36f215b571728b9dad35cfda5c03a25e
andreyscurlatov <andreyscurlatov@gmail.com>
2020-06-21 18:02:18 +0300

    ask.py

--- etc/gunicorn_hello.conf.py
+++ etc/gunicorn_hello.conf.py
@@ -5,3 +5,5 @@ bind = '0.0.0.0:8080'
 workers = 2
 
 pythonpath = '/home/box/web'
+
+	
--- init.sh
+++ init.sh
@@ -2,9 +2,11 @@
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-sudo ln -sf /home/box/web/etc/gunicorn.conf.py   /etc/gunicorn.d/test.py
+sudo ln -sf /home/box/web/etc/gunicorn_hello.conf.py   /etc/gunicorn.d/hello.py
+sudo ln -sf /home/box/web/etc/gunicorn_ask.conf.py   /etc/gunicorn.d/ask.py
 sudo killall -9 gunicorn
-sudo gunicorn -c /etc/gunicorn.d/test.py hello:app &
+sudo gunicorn -c /etc/gunicorn.d/hello.py hello:app &
+sudo gunicorn -c /etc/gunicorn.d/ask.py ask:wsgi &
 #﻿sudo /etc/init.d/mysql start﻿
 
 
