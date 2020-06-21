2f24592d8c639d2e029eaa67762f7b6a7e730c39
andreyscurlatov <andreyscurlatov@gmail.com>
2020-06-21 18:27:34 +0300

    ask.py

--- init.sh
+++ init.sh
@@ -6,7 +6,7 @@ sudo ln -sf /home/box/web/etc/gunicorn_hello.conf.py   /etc/gunicorn.d/hello.py
 sudo ln -sf /home/box/web/etc/gunicorn_ask.conf.py   /etc/gunicorn.d/ask.py
 sudo killall -9 gunicorn
 sudo gunicorn -c /etc/gunicorn.d/hello.py hello:app &
-sudo gunicorn -c /etc/gunicorn.d/ask.py ask.wsgi:application &
+sudo gunicorn -c /etc/gunicorn.d/ask.py wsgi:application &
 #﻿sudo /etc/init.d/mysql start﻿
 
 
