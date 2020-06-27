70bb05476e50366359b0835bb9ae945d2bd488af
andreyscurlatov <andreyscurlatov@gmail.com>
2020-06-27 19:45:16 +0300

    Add new python2 files

--- init.sh
+++ init.sh
@@ -9,7 +9,7 @@ sudo gunicorn -c /etc/gunicorn.d/hello.py hello:app &
 sudo gunicorn -c /etc/gunicorn.d/ask.py ask.wsgi:application &
 
 sudo /etc/init.d/mysql start
-mysql -uroot -e "create database stepic_web;"
+mysql -uroot -e "create database stepic_web character set utf8;"
 mysql -uroot -e "grant all privileges on stepic_web.* to 'box'@'localhost' with grant option;"
 python ~/web/ask/manage.py makemigrations
 python ~/web/ask/manage.py migrate
